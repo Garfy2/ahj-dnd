c676bd80309193c6646cebb16afe8b5c5e7f552b
Garfy2 <105547060+Garfy2@users.noreply.github.com>
2023-05-28 14:39:23 +0300

    pages
    pages

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import Trello from './engine/trello';
-
-export default function app() {
-  const trello = new Trello();
-  trello.init();
-}
-
-app();
--- src/js/data/formErrors.js
+++
@@ -1,7 +0,0 @@
-const FORM_ERRORS = {
-  name: {
-    valueMissing: 'Add card name, please',
-  },
-};
-
-export default { FORM_ERRORS };
